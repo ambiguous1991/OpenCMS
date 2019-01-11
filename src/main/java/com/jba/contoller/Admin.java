8d237dca38d658094122d605f07b56f68cdb82bf
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2019-01-11 16:34:57 +0100

    Some ground work

---
+++ src/main/java/com/jba/contoller/Admin.java
@@ -0,0 +1,13 @@
+package com.jba.contoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Admin {
+
+    @RequestMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+}
