6fedae5c6bad1fef267542d85cba49e51927dd8e
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2019-01-11 16:45:42 +0100

    Some further ground work

--- src/main/java/com/jba/contoller/Admin.java
+++ src/main/java/com/jba/contoller/Admin.java
@@ -4,10 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin")
 public class Admin {
 
-    @RequestMapping("/admin")
+    @RequestMapping
     public String admin(){
         return "admin";
     }
+
+    @RequestMapping("/dashboard")
+    public String dashboard() {return "admin-dashboard";}
 }
