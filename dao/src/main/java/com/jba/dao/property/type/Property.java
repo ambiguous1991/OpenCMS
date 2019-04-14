ee23f6324d41d6c2366d17690082431a4d5e39a1
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2019-04-14 15:23:13 +0200

    Even more ground work

---
+++ dao/src/main/java/com/jba/dao/property/type/Property.java
@@ -0,0 +1,25 @@
+package com.jba.dao.property.type;
+
+import com.jba.dao.type.DAOBase;
+import lombok.Data;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="properties")
+@Data
+@ToString(callSuper = true)
+public class Property extends DAOBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    long id;
+
+    @Column(name="NAME")
+    String name;
+
+    @Column(name = "VALUE")
+    String value;
+}
---
+++ dao/src/test/java/com/jba/dao/configuration/DataSourceConfig.java
@@ -0,0 +1,51 @@
+package com.jba.dao.configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+    protected Logger logger = LogManager.getLogger(getClass());
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoConfiguration.class);
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory(DataSource dataSource){
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setPackagesToScan("com.jba.dao");
+        sessionFactory.setMappingResources("hibernate.cfg.xml");
+        return sessionFactory;
+    }
+
+    @Bean
+    public DataSource dataSource(Environment env){
+        HikariConfig config = new HikariConfig();
+        logger.info("JDBC URL "+env.getProperty("jdbc.test.url"));
+        config.setJdbcUrl(env.getProperty("jdbc.test.url"));
+        config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        config.setUsername(env.getProperty("jdbc.username"));
+        config.setPassword(env.getProperty("jdbc.password"));
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory){
+        HibernateTransactionManager manager = new HibernateTransactionManager();
+        manager.setSessionFactory(sessionFactory);
+        return manager;
+    }
+}
