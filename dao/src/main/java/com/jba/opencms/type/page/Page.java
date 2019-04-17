3b847a6b1e0eb7ed53c44858f79d83df5c06a8ff
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2019-04-17 21:11:33 +0200

    Some changes to Page

--- dao/src/main/java/com/jba/opencms/type/page/Page.java
+++ dao/src/main/java/com/jba/opencms/type/page/Page.java
@@ -1,9 +1,11 @@
 package com.jba.opencms.type.page;
 
 import com.jba.opencms.type.base.BaseTypeSimpleKey;
+import com.jba.opencms.type.image.Image;
 import com.jba.opencms.type.user.Authority;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -30,7 +32,26 @@ public class Page extends BaseTypeSimpleKey<Page> {
             joinColumns = {@JoinColumn(name = "FK_AUTHORITY_ID")},
             inverseJoinColumns = {@JoinColumn(name="FK_PAGE_ID")}
     )
-    protected List<Authority> pageAuthorities = new ArrayList<>();
+    @ToString.Exclude
+    private List<Authority> pageAuthorities = new ArrayList<>();
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "SUBPAGE",
+            joinColumns = {@JoinColumn(name = "FK_PAGE_ID_CHILD")},
+            inverseJoinColumns = {@JoinColumn(name = "FK_PAGE_ID_PARENT")}
+    )
+    @ToString.Exclude
+    private List<Page> subpages = new ArrayList<>();
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "IMAGE_PAGE",
+            joinColumns = {@JoinColumn(name = "FK_IMAGE_ID")},
+            inverseJoinColumns = {@JoinColumn(name = "FK_PAGE_ID")}
+    )
+    @ToString.Exclude
+    private List<Image> images = new ArrayList<>();
 
     public void addAuthority(Authority authority){
         if(!pageAuthorities.contains(authority)) {
---
+++ web/src/main/java/com/jba/opencms/web/security/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.jba.opencms.web.security;
+
+public class SecurityConfig {
+}
