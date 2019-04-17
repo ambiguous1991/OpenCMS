cfa05fe09823ab29e90014aa614c907a80164017
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2019-04-17 21:12:16 +0200

    Created web security config

--- web/src/main/java/com/jba/opencms/web/security/SecurityConfig.java
+++ web/src/main/java/com/jba/opencms/web/security/SecurityConfig.java
@@ -1,4 +1,8 @@
 package com.jba.opencms.web.security;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 }
