9cc5721249b38ff3b9088b4c779222f893c4712b
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2019-04-20 23:42:20 +0200

    Revert "Merge branch 'feature/plainpasswordencodertest' into develop"
    This reverts commit 296e7df1eca2778c3c9c3f89e2c484c13801ed9b.

--- security/src/main/java/com/jba/opencms/security/encoder/PlainPasswordEncoder.java
+++ security/src/main/java/com/jba/opencms/security/encoder/PlainPasswordEncoder.java
@@ -1,12 +1,8 @@
 package com.jba.opencms.security.encoder;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class PlainPasswordEncoder implements PasswordEncoder {
-    private Logger logger = LoggerFactory.getLogger(getClass());
-
     @Override
     public boolean upgradeEncoding(String encodedPassword) {
         return false;
@@ -19,10 +15,6 @@ public class PlainPasswordEncoder implements PasswordEncoder {
 
     @Override
     public boolean matches(CharSequence rawPassword, String encodedPassword) {
-        if(rawPassword == null || rawPassword.length()==0){
-            logger.warn("Input password is empty!");
-        }
-
         return rawPassword.equals(encodedPassword);
     }
 }
