3f76fc99cd95bfc025602f4ee4480410b8722801
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2019-04-21 10:59:33 +0200

    Dashboard controller

---
+++ web/src/main/java/com/jba/opencms/web/controller/DashboardController.java
@@ -0,0 +1,15 @@
+package com.jba.opencms.web.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+
+    @GetMapping("/dashboard")
+    public String dashboard(){
+        return "dashboard";
+    }
+
+}
--- web/src/main/java/com/jba/opencms/web/security/SecurityConfig.java
+++ web/src/main/java/com/jba/opencms/web/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.jba.opencms.web.security;
 
 import com.jba.opencms.security.service.UserDetailsProvider;
+import com.jba.opencms.type.user.enu.AuthorityEnum;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
@@ -37,7 +38,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .anyRequest().authenticated()
-        .and()
+                .antMatchers("/dashboard/**").hasAnyRole(
+                    AuthorityEnum.Administrator.name(),
+                    AuthorityEnum.Editor.name(),
+                    AuthorityEnum.Reviewer.name()
+                )
+                .and()
         .formLogin()
                 .loginPage("/login")
                 .loginProcessingUrl("/login")
