c1ed63c4e0bfdfd8410ec0bc9e4141d91374e824
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2019-04-22 11:38:17 +0200

    Logout

--- web/src/main/java/com/jba/opencms/web/controller/LoginController.java
+++ web/src/main/java/com/jba/opencms/web/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.jba.opencms.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class LoginController {
@@ -9,5 +10,10 @@ public class LoginController {
     @GetMapping("/login")
     public String loginForm(){
         return "login-page";
+    }
+
+    @GetMapping("/logout")
+    public RedirectView redirectView(){
+        return new RedirectView("/");
     }
 }
