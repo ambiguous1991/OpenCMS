bfc2b882ab9044b28c232a7be36d2dc80240825f
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2019-05-11 17:57:58 +0200

    Breadcrumb creator

--- web/src/main/java/com/jba/opencms/web/WebApplication.java
+++ web/src/main/java/com/jba/opencms/web/WebApplication.java
@@ -2,20 +2,27 @@ package com.jba.opencms.web;
 
 import com.jba.opencms.configuration.DataSourceConfiguration;
 import com.jba.opencms.configuration.ServicesInitializr;
+import com.jba.opencms.web.controller.interceptor.BreadcrumbInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class, LiquibaseAutoConfiguration.class})
 @ComponentScan("com.jba.opencms")
 @Import({DataSourceConfiguration.class, ServicesInitializr.class})
-public class WebApplication {
+public class WebApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new BreadcrumbInterceptor());
+    }
 }
---
+++ web/src/main/java/com/jba/opencms/web/controller/interceptor/BreadcrumbInterceptor.java
@@ -0,0 +1,62 @@
+package com.jba.opencms.web.controller.interceptor;
+
+import com.jba.opencms.web.type.Breadcrumb;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Component
+public class BreadcrumbInterceptor extends HandlerInterceptorAdapter {
+
+    private Logger logger = LoggerFactory.getLogger(BreadcrumbInterceptor.class);
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        String requestURI = request.getRequestURI();
+        if(requestURI.startsWith("/dashboard")&&!requestURI.contains("/static")){
+            logger.info("Building breadcrumbs for "+request.getRequestURI());
+            if(requestURI.contains("?"))
+                modelAndView.addObject("breadcrumb", requestToBreadcrumb(requestURI.substring(0, requestURI.lastIndexOf("?"))));
+            else
+                modelAndView.addObject("breadcrumb", requestToBreadcrumb(requestURI));
+        }
+    }
+
+    private List<Breadcrumb> requestToBreadcrumb(final String requestURI){
+        StringBuilder hrefBuilder = new StringBuilder();
+        List<Breadcrumb> collect = Arrays.stream(requestURI.split("/"))
+                .filter(element -> !element.equals(""))
+                .map(element-> {
+                    Breadcrumb breadcrumb = null;
+                    if(element.matches("\\d+"))
+                        breadcrumb = new Breadcrumb(mapping().get("digit"), extractHref(element, hrefBuilder));
+                    else {
+                        breadcrumb = new Breadcrumb(mapping().get(element), extractHref(element, hrefBuilder));
+                    }
+                    return breadcrumb;
+                })
+                .collect(Collectors.toList());
+        return collect;
+    }
+
+    private String extractHref(String element, StringBuilder requestURI){
+        requestURI.append("/"+element);
+        return requestURI.toString();
+    }
+
+    private final static Map<String, String> mapping(){
+        HashMap<String, String> map = new HashMap<>();
+        map.put("dashboard", "Tablica");
+        map.put("menu", "Edycja menu");
+        map.put("digit", "Element");
+        map.put("delete", "Usunięcie");
+        return map;
+    }
+}
