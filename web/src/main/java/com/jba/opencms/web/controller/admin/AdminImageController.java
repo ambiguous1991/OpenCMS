19f082940906992b138990dd4902c0e60d0b5f1a
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2019-05-21 20:57:39 +0200

    Saving image to database

--- web/src/main/java/com/jba/opencms/web/controller/admin/AdminImageController.java
+++ web/src/main/java/com/jba/opencms/web/controller/admin/AdminImageController.java
@@ -1,7 +1,9 @@
 package com.jba.opencms.web.controller.admin;
 
+import com.jba.opencms.image.ImageService;
 import com.jba.opencms.type.image.Image;
 import com.mysql.cj.jdbc.Blob;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -17,6 +19,9 @@ import java.io.IOException;
 @Controller
 @RequestMapping(value = "/dashboard/images")
 public class AdminImageController {
+
+    @Autowired
+    private ImageService imageService;
 
     @RequestMapping(method = RequestMethod.GET)
     public String getImageDashboard() {
@@ -39,8 +44,20 @@ public class AdminImageController {
         try {
             image.setData(file.getBytes());
             image.setExtension(file.getOriginalFilename());
-
-        } catch (IOException e) {
+            String originalFilename = file.getOriginalFilename()==null?"":file.getOriginalFilename();
+            if(originalFilename.contains(".")){
+                String extension = originalFilename.substring(originalFilename.lastIndexOf(".")+1);
+                String title = originalFilename.substring(0, originalFilename.lastIndexOf("."));
+                image.setExtension(extension.toUpperCase());
+                image.setName(title);
+            }
+            else{
+                image.setExtension("NIL");
+                image.setName("none");
+            }
+            imageService.create(image);
+        }
+        catch (IOException e) {
             e.printStackTrace();
             return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
         }
