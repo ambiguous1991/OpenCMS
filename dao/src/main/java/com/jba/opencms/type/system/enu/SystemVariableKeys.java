54ee08b6403885aa1a779650b6b61ff537f77dce
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2019-06-07 23:45:14 +0200

    View fixes

--- dao/src/main/java/com/jba/opencms/type/system/enu/SystemVariableKeys.java
+++ dao/src/main/java/com/jba/opencms/type/system/enu/SystemVariableKeys.java
@@ -1,6 +1,13 @@
 package com.jba.opencms.type.system.enu;
 
-public class SystemVariableKeys {
+import java.util.HashMap;
+
+public class SystemVariableKeys extends HashMap<String, String> {
     public final static String PAGE_NAME = "page.name";
     public final static String SYSTEM_DASHBOARD_DATE_FORMAT ="system.dashboard.dateformat";
+
+    public SystemVariableKeys() {
+        put("PAGE_NAME", "page.name");
+        put("SYSTEM_DASHBOARD_DATE_FORMAT", "system.dashboard.dateformat");
+    }
 }
