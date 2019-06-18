6ba3232e54554c522c2f1047660b1f8c134dccbf
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2019-06-18 15:31:34 +0200

    Fix

--- web/src/main/java/com/jba/opencms/web/controller/admin/users/form/ChangePasswordForm.java
+++ web/src/main/java/com/jba/opencms/web/controller/admin/users/form/ChangePasswordForm.java
@@ -1,4 +1,18 @@
 package com.jba.opencms.web.controller.admin.users.form;
 
+import com.jba.opencms.web.controller.admin.users.validators.PasswordEquals;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@PasswordEquals
 public class ChangePasswordForm {
+    @NotNull private String password;
+    @NotNull private String newPassword;
+    @NotNull private String newPasswordRepeated;
 }
