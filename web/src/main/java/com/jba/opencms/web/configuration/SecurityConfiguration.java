2c72f1186a9873746535d515a733f42e84db9eec
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2019-07-07 16:15:40 +0200

    Misc changes

--- web/src/main/java/com/jba/opencms/web/configuration/SecurityConfiguration.java
+++ web/src/main/java/com/jba/opencms/web/configuration/SecurityConfiguration.java
@@ -66,6 +66,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         super.configure(web);
-        web.ignoring().antMatchers( "/webjars/**", "/css/**", "/js/**", "/image/**", "/images/**", "**/favicon.ico");
+        web.ignoring().antMatchers( "/webjars/**", "/css/**", "/js/**", "/image/**", "/images/**", "/**/favicon.ico");
     }
 }
