9b9334c53773fc9a73a88bb650c23c1d82a4cd44
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2019-07-07 12:09:02 +0200

    Working password change

--- web/src/main/java/com/jba/opencms/web/controller/admin/users/UsersController.java
+++ web/src/main/java/com/jba/opencms/web/controller/admin/users/UsersController.java
@@ -1,6 +1,7 @@
 package com.jba.opencms.web.controller.admin.users;
 
 import com.jba.opencms.security.SecurityUtils;
+import com.jba.opencms.security.service.UserDetailsServicePasswordChangeCapable;
 import com.jba.opencms.type.user.User;
 import com.jba.opencms.web.controller.admin.users.form.ChangePasswordForm;
 import com.jba.opencms.web.controller.admin.users.form.UsernamePasswordForm;
@@ -10,13 +11,13 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -27,9 +28,9 @@ public class UsersController {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     private PasswordEncoder passwordEncoder;
-    private UserDetailsService userDetailsService;
+    private UserDetailsServicePasswordChangeCapable userDetailsService;
 
-    public UsersController(PasswordEncoder passwordEncoder, @Qualifier("userDetailsProvider") UserDetailsService userDetailsService) {
+    public UsersController(PasswordEncoder passwordEncoder, @Qualifier("userDetailsProvider") UserDetailsServicePasswordChangeCapable userDetailsService) {
         this.passwordEncoder = passwordEncoder;
         this.userDetailsService = userDetailsService;
     }
@@ -51,6 +52,12 @@ public class UsersController {
         model.addAttribute("form", form);
 
         return "dashboard/users/change-password";
+    }
+
+    @RequestMapping(value = "/change-password", method = RequestMethod.POST)
+    public RedirectView doPasswordChange(ChangePasswordForm form){
+        userDetailsService.changePassword(form.getUsername(), form.getNewPassword());
+        return new RedirectView("/dashboard/users");
     }
 
     @RequestMapping(value = "/change-password-validate", method = RequestMethod.PATCH)
