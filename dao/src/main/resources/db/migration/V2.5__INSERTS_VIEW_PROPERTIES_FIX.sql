71015f77432bf4f60e891c7da91954f07bb4839d
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2019-07-13 18:41:07 +0200

    Fixes

---
+++ dao/src/main/resources/db/migration/V2.5__INSERTS_VIEW_PROPERTIES_FIX.sql
@@ -0,0 +1,7 @@
+UPDATE system_variable
+SET SYSTEM_VALUE='/resources/css/opencms.css'
+WHERE SYSTEM_KEY='page.presentation.global.css.name';
+
+UPDATE system_variable
+SET SYSTEM_VALUE='/resources/css/theme.css'
+WHERE SYSTEM_KEY='page.presentation.theme.css.name';
