26017d6b931dbcc45d451728626c843f7160c06d
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2019-07-14 18:09:29 +0200

    Fully working css edition

--- web/src/main/java/com/jba/opencms/web/controller/admin/PresentationController.java
+++ web/src/main/java/com/jba/opencms/web/controller/admin/PresentationController.java
@@ -1,17 +1,17 @@
 package com.jba.opencms.web.controller.admin;
 
+import com.jba.opencms.web.repository.FileAccessMode;
 import com.jba.opencms.web.repository.FileRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 
 @Controller
 @RequestMapping(value = "/dashboard/presentation")
@@ -34,5 +34,12 @@ public class PresentationController {
         String content = FileCopyUtils.copyToString(new InputStreamReader(inputStream));
         model.addAttribute("content", content);
         return "dashboard/presentation/edit-css";
+    }
+
+    @RequestMapping(method= RequestMethod.PUT, value = "/edit-css")
+    public ResponseEntity update(@RequestParam String file, String content){
+        repository.save(file, new ByteArrayInputStream(content.getBytes()), FileAccessMode.PUBLIC_READ_ONLY);
+
+        return ResponseEntity.ok("{\n\t\"status\":\"ok\"\n}");
     }
 }
