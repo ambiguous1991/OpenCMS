b0938dc6ae5310feb844a8d25f4843e0fd1c0961
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2019-10-26 23:52:37 +0200

    Files mapped

--- web/src/main/java/com/jba/opencms/web/controller/admin/files/FilesController.java
+++ web/src/main/java/com/jba/opencms/web/controller/admin/files/FilesController.java
@@ -1,16 +1,22 @@
 package com.jba.opencms.web.controller.admin.files;
 
 import com.jba.opencms.file.FileService;
+import com.jba.opencms.type.file.File;
 import com.jba.opencms.type.file.projection.FileProjection;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
 @RequestMapping(value = "/dashboard/files")
+@Slf4j
 public class FilesController {
+
+    private final static String TO_REMOVE_FROM_PATH = "/dashboard/files/file";
 
     private FileService fileService;
 
@@ -25,5 +31,17 @@ public class FilesController {
         model.addAttribute("files", fileMetadata);
 
         return "dashboard/files/files";
+    }
+
+    @RequestMapping(value = "file/**")
+    public String getFileDetails(HttpServletRequest request, Model model){
+        String requestURI = request.getRequestURI();
+        String filePath = requestURI.replaceAll(TO_REMOVE_FROM_PATH, "");
+
+        File file = fileService.get(filePath);
+        model.addAttribute("file", file);
+
+        log.info(request.getRequestURI());
+        return "error/not-found";
     }
 }
