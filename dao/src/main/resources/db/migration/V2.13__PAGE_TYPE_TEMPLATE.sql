d805a7e601d9fc14a7067d3dc6f2863dddf26975
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2020-01-14 19:46:33 +0100

    Fixes

--- dao/src/main/resources/db/migration/V2.13__PAGE_TYPE_TEMPLATE.sql
+++ dao/src/main/resources/db/migration/V2.13__PAGE_TYPE_TEMPLATE.sql
@@ -4,5 +4,5 @@ alter table page_type drop column FK_FILE_ID;
 rename table page_type to template;
 alter table page drop foreign key FK_PAGE_TYPE_ID;
 alter table page change FK_PAGE_TYPE_ID FK_TEMPLATE_ID bigint null;
-alter table page add constraint FK_TEMPLATE_ID foreign key (FK_TEMPLATE_ID) references opencms.template (ID);
+alter table page add constraint FK_TEMPLATE_ID foreign key (FK_TEMPLATE_ID) references template (ID);
 
